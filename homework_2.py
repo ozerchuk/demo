fba92ba6968caa74197bf43d58e14aa70e3fa9a2
viktor_ozerchuk <ozerchuk22@ukr.net>
2023-01-31 17:53:37 +0200

    homework3

--- homework_2.py
+++ homework_2.py
@@ -2,19 +2,17 @@
 #Task 1
 
 
-
-#second case
 name = input('Enter your name:')
 string = len(name)
 print('Word \'', name,'\' consists of', string, 'letters')
 
 #Task 2
 
-age = input('Enter you age:')
+age = input('Enter you age! it should be a number:')
 
 if age.isdigit():
     age_numeric = int(age)
-    if  age_numeric :
+    if  age_numeric < 1:
         print('Less than 1')
     elif age_numeric > 100:
         print ('More than 100')
@@ -22,9 +20,9 @@ if age.isdigit():
         print('You are lucky')
     elif age_numeric < 7:
         print('Where is your parents?')
-    elif age < 16:
+    elif age_numeric < 16:
         print('It is film for adult')
-    elif age > 65:
+    elif age_numeric > 65:
         print('Show your pension certificate')
     else:
         print('There are no tickets')
---
+++ homework_3.py
@@ -0,0 +1,1 @@
+kss s'fdsf '
